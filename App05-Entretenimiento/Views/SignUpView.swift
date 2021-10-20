e94cebc334b10f03980242aec8c17b7e2ae5894a
Pedro Treviño <57535686+PedroTreeev@users.noreply.github.com>
2021-10-20 14:03:59 -0500

    Update SignUpView.swift
    Diseño

--- App05-Entretenimiento/Views/SignUpView.swift
+++ App05-Entretenimiento/Views/SignUpView.swift
@@ -11,30 +11,64 @@ struct SignUpView: View {
     
     @State var username : String = ""
     @State var password : String = ""
+    @Binding var show : Bool
     
     @EnvironmentObject var loginVM : LoginViewModel
     
     var body: some View {
-
-        
-        
-        VStack{
-            TextField("username", text: $username)
-            SecureField("password", text: $password)
-            
-            Button(action: {
-                loginVM.signup(username: username, password: password)
-                
-            }, label: {
-                Text("Sign Up")
-            })
-            
-        }
+        NavigationView{
+            ZStack{
+                Color("ColorDeFondo")
+                    .ignoresSafeArea(.all)
+                VStack{
+                    TextField("Usuario", text: $username)
+                        .frame(width: 300, height: 40)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .autocapitalization(.none)
+                    SecureField("Contraseña", text: $password)
+                        .frame(width: 300, height: 40)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .autocapitalization(.none)
+                    
+                    Button(action: {
+                        if(username.count >= 4 && password.count >= 8){
+                            loginVM.signup(username: username, password: password)
+                            show.toggle()
+                        }
+                    }, label: {
+                        Text("Registrate")
+                            .fontWeight(.semibold)
+                            .frame(width: 100, height: 50)
+                            .background(Color("ColorRosa"))
+                            .clipShape(RoundedRectangle(cornerRadius: 90))
+                            .foregroundColor(.white)
+                            .hoverEffect(.highlight)
+                            .padding(.bottom)
+                    })
+                    
+                    Text("Consideraciones")
+                        .fontWeight(.bold)
+                        .padding(.top)
+                        .padding(.bottom)
+                    Text("La longitud de la contraseña debe tener una longitud minima de 8 caracteres \nEl nombre de usuario debe de contener al menos 4 caracteres")
+                        .fontWeight(.regular)
+                        .frame(width: 300, height: 125, alignment: .top)
+                        .multilineTextAlignment(.center)
+                } // VStack
+            } // ZStack
+                .navigationBarTitle(Text("Unete a MARCO"), displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {self.show.toggle()}, label: {
+                    Image(systemName: "xmark.circle")
+                        .foregroundColor(.white)
+                }))
+        } // NavView
     }
 }
 
 struct SignUpView_Previews: PreviewProvider {
     static var previews: some View {
-        SignUpView()
+        SignUpView(show: .constant(true))
     }
 }
